e3c4f961b0c0ff0f0749b96c9754b12e334a15bd
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2018-03-07 23:03:59 -0500

    Create list model

--- controller/todoList.js
+++ controller/todoList.js
@@ -1,4 +1,5 @@
 const todoList = require('../controller/todoList');
+const list = require('../model/list');
 
 //Require the express package and use express.Router()
 const express = require('express');
@@ -6,20 +7,50 @@ const router = express.Router();
 
 //GET HTTP method to /todoList
 router.get('/',(req,res) => {
-    res.send("GET");
+    list.getAllLists((err, lists)=> {
+        if(err) {
+            res.json({success:false, message: `Failed to load all lists. Error: ${err}`});
+        }
+        else {
+            res.write(JSON.stringify({success: true, lists:lists},null,2));
+            res.end();
+        }
+    });
 });
 
 //POST HTTP method to /todoList
-
 router.post('/', (req,res,next) => {
-    res.send("POST");
+    let newList = new list({
+        description: req.body.description,
+        isChecked: req.body.isChecked,
+        date: req.body.date
+    });
+    list.addList(newList,(err, list) => {
+        if(err) {
+            res.json({success: false, message: `Failed to create a new list. Error: ${err}`});
 
+        }
+        else
+            res.json({success:true, message: "Added successfully."});
+
+    });
 });
 
 //DELETE HTTP method to /todoList. Here, we pass in a params which is the object id.
 router.delete('/:id', (req,res,next)=> {
-    res.send("DELETE");
-
-})
+    //access the parameter which is the id of the item to be deleted
+    let id = req.params.id;
+    //Call the model method deleteListById
+    list.deleteListById(id,(err,list) => {
+        if(err) {
+            res.json({success:false, message: `Failed to delete the list. Error: ${err}`});
+        }
+        else if(list) {
+            res.json({success:true, message: "Deleted successfully"});
+        }
+        else
+            res.json({success:false});
+    })
+});
 
 module.exports = router;
---
+++ model/list.js
@@ -0,0 +1,34 @@
+//Require mongoose package
+const mongoose = require('mongoose');
+
+//Define TodoListSchema with description, isChecked, date
+const Schema = mongoose.Schema;
+const TodoListSchema = Schema({
+    description: {
+        type: String,
+        required: true
+    },
+    isChecked: Boolean,
+    date: {
+        type: Date,
+        required: true
+    }
+});
+const TodoList = mongoose.model('TodoList', TodoListSchema );
+module.exports = TodoList;
+
+//TodoList.find() returns all the lists
+module.exports.getAllLists = (callback) => {
+    TodoList.find(callback);
+}
+
+//newList.save is used to insert the document into MongoDB
+module.exports.addList = (newList, callback) => {
+    newList.save(callback);
+}
+
+//Here we need to pass an id parameter to TodoList.remove
+module.exports.deleteListById = (id, callback) => {
+    let query = {_id: id};
+    TodoList.remove(query, callback);
+}
